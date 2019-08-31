3588aaeee41ee8fbb7c52f09703be1cf0469ee54
sbx_user1051 <sbx_user1051@169.254.238.109>
2019-08-31 13:58:08 +0000

    Sat Aug 31 13:58:08 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,12 +1,27 @@
+import collections
 import unittest
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    itemPrices = {}
+    itemPrices['A'] = {1:50, 3:130}
+    itemPrices['B'] = {1:30, 2:45}
+    itemPrices['C'] = {1:20}
+    itemPrices['D'] = {1:15}
 
-    specialOffers = {'3A':130, '2B':45}
-    itemPrices = {'A':50, 'B':30, 'C':20, 'D':15}
-    return itemPrices.get(skus)
+    itemCounts = collections.defaultdict(int)
+    for item in skus:
+        itemCounts[item] += 1
+
+    totalCost = 0
+    for item, count in itemCounts.items():
+        if count in itemPrices[item]:
+            totalCost += itemPrices[item][count]
+        else:
+            totalCost += count * itemPrices[item][1]
+
+    return totalCost
 
 
 class TestCheckOut(unittest.TestCase):
@@ -25,8 +40,15 @@ class TestCheckOut(unittest.TestCase):
     def test_specialOfferAPriceOnly(self):
         self.assertEqual(checkout('AAA'), 130)
 
+    def test_specialOfferBPriceOnly(self):
+        self.assertEqual(checkout('BB'), 45)
+
+    def test_mixedSingleItems(self):
+        self.assertEqual(checkout("ABCD"), checkout("A") + checkout("B") + checkout("C") + checkout("D"))
+
 
 
 
 if __name__ == '__main__':
     unittest.main()
+
