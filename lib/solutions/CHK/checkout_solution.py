828bb4ed62d9c29367eb8067e9c442d2d6c409fc
sbx_user1051 <sbx_user1051@169.254.238.109>
2019-08-31 13:43:08 +0000

    Sat Aug 31 13:43:08 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,23 @@
-
+import unittest
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    print(skus)
+
+    specialOffers = {'3A':130, '2B':45}
+    itemPrices = {'A':50, 'B':30, 'C':20, 'D':15}
+    return itemPrices.get(skus)
+
+
+class TestCheckOut(unittest.TestCase):
+    def test_singleAPrice(self):
+        self.assertEqual(checkout('A'), 50)
+
+    def test_singleBPrice(self):
+        self.assertEqual(checkout('B'), 30)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
 
